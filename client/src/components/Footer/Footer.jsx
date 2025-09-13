aabcf2403957638a7bbe8e8972f05b855db12e6e
shahirsizan <shahir.sizan18@gmail.com>
2025-09-13 10:53:32 +0600

    Footer modified

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,74 +1,68 @@
-import React, { useState } from "react";
 import { FaLocationArrow, FaMobileAlt } from "react-icons/fa";
 import footerLogo from "../../assets/food-logo.png";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
 	const year = new Date().getFullYear();
-	// console.log(year);
 
 	return (
-		<div className="bg-gray-100 dark:bg-gray-950">
-			<section className="max-w-[1200px] mx-auto">
-				<div className=" py-5 ">
-					<div className="font-atma text-lg py-3 px-4 flex flex-col sm:flex-row sm:justify-between">
-						<h1 className="text-xl max-sm:m-auto max-sm:text-xl sm:text-4xl font-bold sm:text-left text-justify flex items-center gap-3">
-							<img
-								src={footerLogo}
-								className="max-[320px]:hidden w-10 drop-shadow-[0_1px_1px_black]"
-							/>
-							<span className="max-[320px]:text-2xl drop-shadow-[0_1px_1px_black] bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary/90">
-								খাবারবাড়ি
-							</span>
-						</h1>
+		<section className="footerSection py-5 font-atma">
+			<div className=" text-lg py-3 px-4 flex flex-col sm:flex-row sm:justify-between">
+				<h1 className="max-sm:m-auto font-bold sm:text-left text-justify flex items-center gap-3">
+					<img
+						src={footerLogo}
+						className="w-10 drop-shadow-[1px_1px_0px_black]"
+					/>
+					<span className="text-2xl lg:text-3xl bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary/90 drop-shadow-[1px_1px_0px_black]">
+						খাবারবাড়ি
+					</span>
+				</h1>
 
-						<div className="flex max-sm:m-auto max-sm:text-xl items-center gap-3">
-							<FaLocationArrow />
-							<p className="bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary drop-shadow-[1px_0_1px_black]">
-								Dhaka, Bangladesh
-							</p>
-						</div>
-
-						<div className="flex max-sm:m-auto max-sm:text-xl items-center gap-3">
-							<FaMobileAlt />
-							<p className="bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary drop-shadow-[1px_0_1px_black]">
-								01819991239
-							</p>
-						</div>
-					</div>
-
-					<div>
-						<div className="font-atma text-center py-1 border-t-2 border-gray-300/50">
-							© Copyright {year} All rights reserved
-						</div>
-
-						<div className="font-atma text-center py-1 text-xl border-gray-300/50">
-							Developed by:{" "}
-							<Link
-								to={"https://github.com/shahirsizan"}
-								target="_blank"
-								className="font-semibold bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary drop-shadow-[1px_0_1px_black]"
-							>
-								Shahir Adil Sizan
-							</Link>
-						</div>
-
-						<div className="font-atma text-center py-1 text-xl border-gray-300/50">
-							Source Code:{" "}
-							<Link
-								to={
-									"https://github.com/shahirsizan/khabar-bari-online-food-ordering-app-frontend"
-								}
-								target="_blank"
-								className="font-semibold bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary drop-shadow-[1px_0_1px_black]"
-							>
-								Github
-							</Link>
-						</div>
-					</div>
+				<div className="flex max-sm:m-auto items-center gap-3 font-semibold">
+					<FaLocationArrow className="text-lg" />
+					<p className="text-lg bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary drop-shadow-[1px_1px_0px_black]">
+						Dhaka, Bangladesh
+					</p>
 				</div>
-			</section>
-		</div>
+
+				<div className="flex max-sm:m-auto items-center gap-3 font-semibold">
+					<FaMobileAlt className="text-lg" />
+					<p className="text-lg bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary drop-shadow-[1px_1px_0px_black]">
+						01819991239
+					</p>
+				</div>
+			</div>
+
+			<div>
+				<div className=" text-center py-1 border-t-2 border-gray-200/20">
+					© Copyright {year} All rights reserved
+				</div>
+
+				<div className=" text-center py-1 text-xl border-gray-300/50">
+					Developed by:{" "}
+					<Link
+						to={"https://github.com/shahirsizan"}
+						target="_blank"
+						className="font-semibold bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary drop-shadow-[1px_1px_0px_black]"
+					>
+						<span className="flex-nowrap">Shahir Adil Sizan</span>
+					</Link>
+				</div>
+
+				<div className=" text-center py-1 text-xl border-gray-300/50">
+					Source Code:{" "}
+					<Link
+						to={
+							"https://github.com/shahirsizan/khabar-bari-online-food-ordering-app-frontend"
+						}
+						target="_blank"
+						className="font-semibold bg-clip-text text-transparent bg-gradient-to-b from-primary to-secondary drop-shadow-[1px_1px_0px_black]"
+					>
+						Github
+					</Link>
+				</div>
+			</div>
+		</section>
 	);
 };
 
