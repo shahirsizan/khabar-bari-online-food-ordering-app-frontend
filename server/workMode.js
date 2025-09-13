82d608da7632778f0d5f5ca84890358dc8e4d4dc
shahirsizan <shahir.sizan18@gmail.com>
2025-09-14 01:35:14 +0600

    minor change

--- server/workMode.js
+++ server/workMode.js
@@ -5,7 +5,7 @@ if (mode === "dev") {
 	frontend_base_url = "http://localhost:5173";
 	backend_base_url = "http://localhost:5000";
 } else {
-	frontend_base_url = "https://khabar-bari-client.vercel.app";
+	frontend_base_url = "https://khabar-bari-frontend.vercel.app/";
 	backend_base_url = "https://khabar-bari-backend.onrender.com";
 }
 
