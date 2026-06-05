694004e79ded08828db0bd0188589a555a4a1a93
shahirsizan <shahir.sizan18@gmail.com>
2026-06-05 21:54:34 +0600

    guest and protected layout added

--- client/src/GuestRoute.jsx
+++ client/src/GuestRoute.jsx
@@ -2,10 +2,18 @@ import { Navigate } from "react-router-dom";
 import { useUserContext } from "./UserContext";
 
 const GuestRoute = ({ children }) => {
-	const { isAuthenticated } = useUserContext();
+	const { isInitializing, isAuthenticated } = useUserContext();
 
 	// If authenticated, redirect to home
-	return !isAuthenticated ? children : <Navigate to="/" replace />;
+	return isInitializing ? (
+		<div className="h-screen flex items-center justify-center bg-yellow-500 text-black">
+			Loading...
+		</div>
+	) : !isAuthenticated ? (
+		children
+	) : (
+		<Navigate to="/" replace />
+	);
 };
 
 export default GuestRoute;
--- client/src/ProtectedLayout.jsx
+++ client/src/ProtectedLayout.jsx
@@ -2,10 +2,18 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useUserContext } from "./UserContext";
 
 const ProtectedLayout = () => {
-	const { isAuthenticated } = useUserContext();
+	const { isInitializing, isAuthenticated } = useUserContext();
 
 	// If not authenticated, redirect to login
-	return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+	return isInitializing ? (
+		<div className="h-screen flex items-center justify-center text-4xl bg-yellow-500 text-black">
+			Loading...
+		</div>
+	) : isAuthenticated ? (
+		<Outlet />
+	) : (
+		<Navigate to="/login" replace />
+	);
 };
 
 export default ProtectedLayout;
