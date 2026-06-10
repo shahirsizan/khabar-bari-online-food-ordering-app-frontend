60ead7696563cf19683edcc56bdde2caf7200acc
shahirsizan <shahir.sizan18@gmail.com>
2026-06-10 17:08:00 +0600

    dynamic component for all the recipes for landing page added

--- client/src/components/RecipeList/RecipeList.jsx
+++ client/src/components/RecipeList/RecipeList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import AchariChicken from "../../assets/AchariChicken.png";
 import BeefBrainMasala from "../../assets/BeefBrainMasala.png";
 import chickenkarai from "../../assets/chickenkarai.png";
@@ -14,72 +14,48 @@ import { useLocation } from "react-router-dom";
 
 const RecipeList = () => {
 	const { cartItems, addToCart, updateQuantity, removeFromCart } = useCart();
+	const [menuItems, setMenuItems] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-	const FoodData = [
-		{
-			id: 1,
-			image: AchariChicken,
-			price: 499,
-			name: "আচারি চিকেন",
-			desc: "আম এবং রসুনের আচার দিয়ে বানানো চিকেনের রেসিপি",
-		},
-		{
-			id: 2,
-			image: BeefBrainMasala,
-			price: 599,
-			name: "গরুর মগজ ভূনা",
-			desc: "গরুর মগজ, পেঁয়াজ, রসুন, আদা ও বিভিন্ন মসলা সহযোগে ভুনা করা।",
-		},
-		{
-			id: 3,
-			image: chickenkarai,
-			price: 499,
-			name: "চিকেন কড়াই",
-			desc: "টমেটোর ঘন ও মসলাদার ঝোলে রান্না করা মুরগির মাংস",
-		},
-		{
-			id: 4,
-			image: ChickenTikkaButterMasala,
-			price: 480,
-			name: "চিকেন টিক্কা বাটার মাসালা",
-			desc: "রোস্ট করা মসলাযুক্ত তরকারির সাথে রান্না করা মুরগির মাংস",
-		},
-		{
-			id: 5,
-			image: rice,
-			price: 30,
-			name: "ভাত",
-			desc: "সাদা ভাত",
-		},
-		{
-			id: 6,
-			image: ShahiMorogPolao,
-			price: 153,
-			name: "শাহী মোরগ পোলাও",
-			desc: "মোরগ পোলাও. ডিম ও কাবাব সহযোগে পরিবেশিত। অন্যান্য নাম, শাহী মোরগ পোলাও, ঢাকাইয়া মোরগ পোলাও",
-		},
-		{
-			id: 7,
-			image: EggKhichuri,
-			price: 85,
-			name: "ডিম খিচুড়ি",
-			desc: "ডিম খিচুড়ি",
-		},
-		{
-			id: 8,
-			image: moogdal,
-			price: 40,
-			name: "মুগ ডাল",
-			desc: "মুগ ডাল ও মশলা সহযোগে প্রস্তুতকৃত, প্রোটিন সমৃদ্ধ",
-		},
-		{
-			id: 9,
-			image: mixedvegetable,
-			price: 40,
-			name: "মিক্সড সবজি",
-			desc: "আলু, লাউ, মিষ্টিকুমড়াসহ কয়েক পদের সবজি সহযোগে প্রস্তুতকৃত",
-		},
-	];
+	useEffect(() => {
+		const fetchMenuItems = async () => {
+			try {
+				const response = await fetch(
+					"http://localhost:5000/api/menu-items",
+					{
+						method: "GET",
+						headers: {
+							token: JSON.parse(localStorage.getItem("token")),
+						},
+					},
+				);
+
+				if (response.ok) {
+					const data = await response.json();
+
+					// Map the DB documents to match the UI's expected structure
+					const normalizedItems = data.map((item) => ({
+						id: item._id,
+						name: item.name,
+						price: item.basePrice,
+						desc: item.description,
+						image: item.image,
+					}));
+
+					console.log(normalizedItems);
+
+					setMenuItems(normalizedItems);
+				} else {
+					throw new Error("Error in fetchMenuItems");
+				}
+			} catch (error) {
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchMenuItems();
+	}, []);
 
 	// `cartItems` e already item ta thakle count return koro. Nahole return 0.
 	const getQuantity = (id) => {
@@ -89,19 +65,33 @@ const RecipeList = () => {
 	// scroll to selected-id section
 	const location = useLocation();
 	useEffect(() => {
-		// if there's a hash in the URL
 		if (location.hash) {
-			const id = location.hash.replace("#", "");
-			const element = document.getElementById(id);
-			if (element) {
-				element.scrollIntoView({ behavior: "smooth" });
-			}
+			const targetId = location.hash.substring(1);
+
+			// 1. Add a slight delay (100ms) to allow the DOM/Images to settle
+			const timer = setTimeout(() => {
+				const element = document.getElementById(targetId);
+				if (element) {
+					element.scrollIntoView({ behavior: "smooth" });
+				}
+			}, 200);
+
+			// Cleanup timeout if user navigates away quickly
+			return () => clearTimeout(timer);
 		}
 	}, []);
 
+	if (isLoading) {
+		return (
+			<div className="h-screen flex items-center justify-center text-3xl">
+				Loading...
+			</div>
+		);
+	}
+
 	return (
 		<section
-			className="recipeSection font-atma py-8 lg:py-12 px-[5vw] md:px-[8vw] lg:px-[10vw]"
+			className="recipeList scroll-mt-32 font-atma py-8 lg:py-12 px-[5vw] md:px-[8vw] lg:px-[10vw]"
 			id="recipeList"
 		>
 			{/* header text */}
@@ -113,7 +103,7 @@ const RecipeList = () => {
 
 			{/* CARDS */}
 			<div className="grid grid-cols-fluid-grid gap-3 xl:gap-4">
-				{FoodData.map((item, index) => {
+				{menuItems.map((item, index) => {
 					const quantity = getQuantity(item.id);
 					const toBanglaNumber = (number) => {
 						const banglaDigits = [
@@ -134,29 +124,40 @@ const RecipeList = () => {
 							.map((d) => banglaDigits[parseInt(d)])
 							.join("");
 					};
-					// item = {
-					// 	image: AchariChicken,
-					// 	price: 499,
-					// 	name: "Achari Chicken",
-					// 	desc: "Blend of pickling spices mixed with tender chicken pcs creating a thick & delicious gravy",
-					// }
+
+					/***
+					 * items:
+					 * [
+					 * 		{
+								"id": "6a217440d6b3653f8f161e1e",
+								"name": "চিকেন বিরিয়ানি",
+								"price": 200,
+								"desc": "চিকেন বিরিয়ানি রেসিপি",
+								"image": "https://recipe30.com/wp-content/uploads/2023/03/chicken-Biryani.jpg"
+							}.
+							{},
+							{},
+						]
+}
+					 * 
+					 */
 					return (
 						<div
 							key={index}
 							// nicher dark/bright class gula rakhte hobe. Do not remove
-							className="flex flex-col font-atma bg-gradient-to-r from-primary to-secondary text-white p-2 xl:p-4 rounded-2xl shadow-xl"
+							className="flex flex-col font-atma bg-gradient-to-r from-primary to-secondary text-white p-2 xl:p-4 rounded-2xl shadow-xl group hover:scale-105 transition-all duration-700"
 						>
 							{/* IMAGE CONTAINER */}
 							<div className="w-full h-32 md:h-40 overflow-hidden flex justify-center items-center rounded-2xl shadow-lg">
 								<img
 									src={item.image}
-									className="object-cover"
+									className="object-cover group-hover:scale-110 transition-all duration-1000"
 								/>
 							</div>
 
 							{/* name & desc */}
 							<div className="py-2 xl:py-3">
-								<p className="font-bold text-2xl lg:text-4xl text-center drop-shadow-[1px_1px_0px_black]">
+								<p className="font-bold text-lg lg:text-2xl text-center drop-shadow-[2px_1px_2px_black]">
 									{item.name}
 								</p>
 							</div>
@@ -167,19 +168,17 @@ const RecipeList = () => {
 							<div className="">
 								{/* price */}
 								<div>
-									<p className="font-semibold text-xl lg:text-3xl text-center drop-shadow-[1px_1px_0px_black]">
+									<p className="font-semibold text-md lg:text-3xl text-center drop-shadow-[2px_1px_2px_black]">
 										৳ {toBanglaNumber(item.price)}
 									</p>
 								</div>
 
 								<div>
 									{quantity > 0 ? (
-										<div className="mt-5 flex items-center gap-2 justify-center">
-											{/* Already selected */}
-
+										<div className="WHEN-SELECTED mt-5 md:mt-10 flex items-center gap-2 justify-center">
 											{/* Minus button */}
 											<button
-												className="transition-all hover:scale-110"
+												className="border-none"
 												onClick={() => {
 													quantity > 1
 														? updateQuantity(
@@ -195,7 +194,7 @@ const RecipeList = () => {
 													stroke="currentColor"
 													fill="currentColor"
 													viewBox="0 0 448 512"
-													className=" text-orange-500 dark:text-orange-500 bg-white border-4 border-orange-500 w-7 h-7 rounded-full drop-shadow-[1px_1px_0px_black]"
+													className=" text-orange-500 dark:text-orange-500 bg-white border-4 border-orange-500 w-5 h-5 md:w-7 md:h-7 rounded-full drop-shadow-[1px_1px_0px_black]"
 													xmlns="http://www.w3.org/2000/svg"
 												>
 													<path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
@@ -203,13 +202,13 @@ const RecipeList = () => {
 											</button>
 
 											{/* quantity */}
-											<span className="w-8 text-center font-semibold text-2xl drop-shadow-[1px_1px_0px_black]">
+											<span className="w-8 text-center font-semibold text-2xl drop-shadow-[2px_1px_2px_black]">
 												{toBanglaNumber(quantity)}
 											</span>
 
 											{/* Plus button */}
 											<button
-												className="transition-all hover:scale-110"
+												className="border-none"
 												onClick={() => {
 													updateQuantity(
 														item.id,
@@ -221,7 +220,7 @@ const RecipeList = () => {
 													stroke="currentColor"
 													fill="currentColor"
 													viewBox="0 0 448 512"
-													className="text-orange-500 dark:text-orange-500 bg-white border-4 border-orange-500 w-7 h-7 rounded-full drop-shadow-[1px_1px_0px_black]"
+													className="text-orange-500 dark:text-orange-500 bg-white border-4 border-orange-500 w-5 h-5 md:w-7 md:h-7 rounded-full drop-shadow-[1px_1px_0px_black]"
 													xmlns="http://www.w3.org/2000/svg"
 												>
 													<path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
@@ -229,8 +228,7 @@ const RecipeList = () => {
 											</button>
 										</div>
 									) : (
-										<div className="flex justify-center gap-2">
-											{/* Not selected */}
+										<div className="WHEN-NOT-SELECTED mt-5 md:mt-10 flex justify-center gap-2">
 											<button
 												onClick={() => {
 													addToCart(item, 1);
@@ -239,9 +237,9 @@ const RecipeList = () => {
 													// 		dispatch({ type: "ADD_ITEM", payload: { item, quantity } });
 													// 	}, []);
 												}}
-												className="my-2 py-1 px-4 rounded-full bg-gradient-to-r from-primary to-secondary  text-2xl font-semibold drop-shadow-[1px_1px_2px_black] transition-all hover:scale-110"
+												className="py-1 px-2 rounded-full bg-gradient-to-r from-primary to-secondary drop-shadow-[2px_1px_2px_black]"
 											>
-												<span className="relative z-10 drop-shadow-[1px_1px_2px_gray]">
+												<span className=" text-sm md:text-md xl:text-lg text-center">
 													অর্ডার করুন
 												</span>
 											</button>
