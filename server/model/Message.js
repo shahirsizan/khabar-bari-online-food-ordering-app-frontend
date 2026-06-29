fbba40947a8aec9c73d824082ef406f01ace457e
shahirsizan <shahir.sizan18@gmail.com>
2026-06-29 23:53:21 +0600

    Message model added

---
+++ server/model/Message.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+	{
+		/***
+		 * 1. In a chat, one party is `customer`, and the other is `admin`
+		 * 2. as admin is fixed, so `User ID` should be the `roomId`.
+		 */
+		roomId: { type: String, required: true, index: true },
+		senderId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		senderRole: {
+			type: String,
+			enum: ["customer", "admin"],
+			required: true,
+		},
+		text: { type: String, required: true },
+	},
+	{ timestamps: true },
+);
+
+export const Message = mongoose.model("Message", messageSchema);
