3d847d267c8a833f4bf129efc5aa0b665d24bebb
shahirsizan <shahir.sizan18@gmail.com>
2026-07-09 10:37:16 +0600

    slight modification

--- server/server.js
+++ server/server.js
@@ -34,13 +34,13 @@ app.use(
 // Initializing Socket.io with CORS
 const io = new Server(httpServer, {
 	cors: {
-		origin: `frontend_base_url`,
+		origin: `${frontend_base_url}`,
 		methods: ["GET", "POST"],
 	},
 });
 
 io.on("connection", (socket) => {
-	console.log("A user connected: ", socket.id);
+	// console.log("A user connected: ", socket.id);
 
 	// 1. when user joins a specific room
 	socket.on("join_room", (roomId) => {
@@ -76,14 +76,25 @@ io.on("connection", (socket) => {
 
 const db = async () => {
 	try {
+		mongoose.connection.on("connected", () => {
+			console.log("Connected to database sucessfully");
+		});
+
+		mongoose.connection.on("error", (err) => {
+			console.log("Error while connecting to database :" + err);
+		});
+
+		mongoose.connection.on("disconnected", () => {
+			console.log("Mongodb connection disconnected");
+		});
+
 		await mongoose.connect(process.env.db_url);
-		console.log("✅ mongodb connected");
 	} catch (error) {
 		console.log("❌ mongodb connection failed: ", error.message);
 	}
 };
 
-app.use(slidingWindowLimiter(15000, 10));
+app.use(slidingWindowLimiter(15000, 15));
 app.use("/api", router);
 app.use("/", (req, res) => {
 	res.send("cron hit");
