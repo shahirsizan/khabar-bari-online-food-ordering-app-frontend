a9994501bc625a9b0a33a976712de571b24e680c
shahirsizan <shahir.sizan18@gmail.com>
2026-07-16 13:20:51 +0600

    Notification model added

---
+++ server/model/Notification.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+	{
+		message: { type: String, required: true },
+		isRead: { type: Boolean, default: false },
+		link: { type: String, required: true }, // URL to redirect after click
+		recipientId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		}, // The user or admin
+		type: {
+			type: String,
+			enum: ["order", "chat", "system"],
+			required: true,
+		},
+	},
+	{ timestamps: true },
+);
+
+export const Notification = mongoose.model("Notification", notificationSchema);
