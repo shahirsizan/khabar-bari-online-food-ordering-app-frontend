1fa216e0833694568732454bb9f818a93854d90d
shahirsizan <shahir.sizan18@gmail.com>
2026-08-10 23:17:56 +0600

    upload image through pre-signed url for better optimization (Bypass server, send to Cloudinary directly)

--- server/controller/uploadController.js
+++ server/controller/uploadController.js
@@ -1,37 +1,28 @@
 import { v2 as cloudinary } from "cloudinary";
 import streamifier from "streamifier";
+import "dotenv/config";
 
-export const uploadImage = async (req, res) => {
-	cloudinary.config({
-		cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-		api_key: process.env.CLOUDINARY_API_KEY,
-		api_secret: process.env.CLOUDINARY_API_SECRET,
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const getPresignedSignature = async (req, res) => {
+	const timestamp = Math.round(new Date().getTime() / 1000);
+	const upload_preset = "khabarbari-upload-img-browser-to-cloudinary";
+
+	// Generate the cryptographic signature
+	const signature = cloudinary.utils.api_sign_request(
+		{ timestamp, upload_preset },
+		cloudinary.config().api_secret,
+	);
+
+	res.json({
+		signature: signature,
+		timestamp: timestamp,
+		upload_preset: upload_preset,
+		apiKey: process.env.CLOUDINARY_API_KEY,
+		cloudName: process.env.CLOUDINARY_CLOUD_NAME,
 	});
-
-	try {
-		if (!req.file) {
-			return res.status(400).json({ message: "No file found" });
-		}
-
-		const streamUpload = (fileBuffer) => {
-			return new Promise((resolve, reject) => {
-				const stream = cloudinary.uploader.upload_stream(
-					{ folder: "Khabarbari" },
-					(error, result) => {
-						if (result) {
-							resolve(result);
-						} else {
-							reject(error);
-						}
-					},
-				);
-				streamifier.createReadStream(fileBuffer).pipe(stream);
-			});
-		};
-
-		const result = await streamUpload(req.file.buffer);
-		res.status(200).json({ secure_url: result.secure_url }); // Returning just the URL
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
 };
