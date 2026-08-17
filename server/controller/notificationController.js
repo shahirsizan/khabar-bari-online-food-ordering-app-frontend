54f5c11f5e2118cf6e944bf75d05e18057e14539
shahirsizan <shahir.sizan18@gmail.com>
2026-08-17 22:39:29 +0600

    controller to mark several chat notification at once implemented

--- server/controller/notificationController.js
+++ server/controller/notificationController.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import { ObjectId } from "mongodb";
 import { Notification } from "../model/NotificationModel.js";
 
 export const getNotifications = async (req, res) => {
@@ -18,17 +20,51 @@ export const getNotifications = async (req, res) => {
 export const markNotificationAsRead = async (req, res) => {
 	try {
 		const { id } = req.params;
-		const notification = await Notification.findByIdAndUpdate(
-			id,
-			{ isRead: true },
+
+		const notification = await Notification.findOneAndUpdate(
+			{ _id: id, recipientId: req.user._id },
+			{ $set: { isRead: true } },
 			{ new: true },
 		);
+
 		res.status(200).json({
 			message: "Notification marked as read",
 			notification,
 		});
 	} catch (error) {
 		console.log("Error -> markNotificationAsRead: ", error.message);
-		res.status(500).json({ error: error.message });
+		res.status(500).json({
+			message: "নটিফিকেশন কন্ট্রোলারে সমস্যা হয়েছে।",
+		});
+	}
+};
+
+export const markSeveralChatNotificationsAsRead = async (req, res) => {
+	try {
+		const { link } = req.body;
+
+		// console.log("link: ", link);
+
+		await Notification.updateMany(
+			{
+				recipientId: req.user._id,
+				link: link,
+				isRead: false,
+			},
+			{ $set: { isRead: true } },
+		);
+
+		return res.status(200).json({
+			success: true,
+			message: "Chat notifications marked as read.",
+		});
+	} catch (error) {
+		console.log(
+			"Error -> markSeveralChatNotificationsAsRead: ",
+			error.message,
+		);
+		res.status(500).json({
+			message: "নটিফিকেশন কন্ট্রোলারে সমস্যা হয়েছে।",
+		});
 	}
 };
