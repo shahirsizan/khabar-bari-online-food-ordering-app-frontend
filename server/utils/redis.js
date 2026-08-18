98850aac60ca8b5a00414d3d3b3cfc82641f37e9
shahirsizan <shahir.sizan18@gmail.com>
2026-08-18 20:04:16 +0600

    Upstash Redis implemented for production deployment

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -1,10 +1,25 @@
 import Redis from "ioredis";
+import { mode } from "../workMode.js";
+import "dotenv/config";
 
-const redis = new Redis({
-	host: process.env.REDIS_HOST,
-	port: process.env.REDIS_PORT,
-	family: 4, // Forces IPv4
-});
+/***
+ * For local development, use dockerized redis container.
+ * For production, use Upstash redis connection
+ */
+let redis;
+if (mode === "dev") {
+	redis = new Redis({
+		host: process.env.REDIS_HOST,
+		port: process.env.REDIS_PORT,
+		family: 4, // Forces IPv4
+	});
+} else {
+	redis = new Redis(process.env.REDIS_URL, {
+		tls: {
+			rejectUnauthorized: false,
+		},
+	});
+}
 
 redis.on("connect", () => {
 	console.log("✅ Connected to Redis.");
