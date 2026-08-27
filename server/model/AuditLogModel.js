4a5ae781152a61d700e8ef7f38fee02a08f07e00
shahirsizan <shahir.sizan18@gmail.com>
2026-08-27 22:13:00 +0600

    Model for audit logs created

---
+++ server/model/AuditLogModel.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const auditLogSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+			index: true,
+		},
+		userEmail: {
+			type: String,
+			required: true,
+		},
+		action: {
+			type: String,
+			required: true,
+			enum: [
+				"CREATE_MENU_ITEM",
+				"UPDATE_MENU_ITEM",
+				"DELETE_MENU_ITEM",
+				"PLACE_ORDER",
+				"UPDATE_ORDER_STATUS",
+				"DELETE_USER",
+				"UPDATE_USER",
+				"RESET_USER_PASSWORD",
+			],
+			index: true,
+		},
+		targetEntityType: {
+			type: String,
+			required: true,
+			enum: ["MenuItem", "Order", "User"],
+			index: true,
+		},
+		targetId: {
+			type: String,
+			default: null,
+		},
+		details: {
+			method: { type: String },
+			url: { type: String },
+			urlParams: { type: Object },
+			queryParams: { type: Object },
+			body: { type: Object },
+		},
+		ipAddress: {
+			type: String,
+			default: "Unknown",
+		},
+		userAgent: {
+			type: String,
+			default: "Unknown",
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+/***
+ * Compound index
+ */
+auditLogSchema.index({ createdAt: -1, action: 1 });
+
+export const AuditLog = mongoose.model("AuditLog", auditLogSchema);
