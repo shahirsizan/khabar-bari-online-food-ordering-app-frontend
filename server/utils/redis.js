03f0c14e6aed3044dc1936f821f090ae8b17daa2
shahirsizan <shahir.sizan18@gmail.com>
2026-08-30 11:58:06 +0600

    [improved] Redis instantiation for caching and bullmq

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -8,59 +8,65 @@ import "dotenv/config";
 
 /***
  * FACTORY PATTERN
- * Generate config based on environment
+ * Generate config based on environment and usage type
  */
-const getRedisConfig = (isWorker = false) => {
-	const baseConfig =
-		mode === "dev"
-			? {
-					host: process.env.REDIS_HOST,
-					port: parseInt(process.env.REDIS_PORT),
-					family: 4,
-				}
-			: { tls: { rejectUnauthorized: false } };
+const getRedisConfig = (isBullMq = false) => {
+	const isProd = mode === "prod";
 
-	const connectionString =
-		mode === "prod" ? process.env.REDIS_URL : undefined;
+	const baseConfig = {
+		family: 4,
+		...(isProd
+			? { tls: { rejectUnauthorized: false } }
+			: {
+					host: process.env.REDIS_HOST,
+					port: process.env.REDIS_PORT,
+				}),
+	};
+	const connectionString = isProd ? process.env.REDIS_URL : undefined;
 
 	return {
 		connectionString,
 		options: {
 			...baseConfig,
-			maxRetriesPerRequest: isWorker ? null : 1,
 			/***
-			 *  Workers must have `null`. Normal cache instances can use default or 1 for fast fail.
+			 * BullMQ requires null for BOTH Queue producers and Worker consumers
 			 */
+			maxRetriesPerRequest: isBullMq ? null : 1,
 		},
 	};
 };
 
 /***
  * FACTORY PATTERN
- * Generate redis instance based on purpose
+ * Generate redis instance
  */
-export const createRedisInstance = (isWorker = false) => {
-	const config = getRedisConfig(isWorker);
-	return config.connectionString
+export const createRedisInstance = (isBullMq = false) => {
+	const config = getRedisConfig(isBullMq);
+	const instance = config.connectionString
 		? new Redis(config.connectionString, config.options)
 		: new Redis(config.options);
+
+	return instance;
 };
 
 /***
- * Connection for normal caching
+ * Connection for normal backend caching
  */
 export const redis = createRedisInstance(false);
+redis.on("connect", () => console.log("✅ Connected to Redis Cache."));
 
 /***
- * Connection for BullMQ workers (consumers).
+ * Dedicated connection for BullMQ Queues (Producers)
+ */
+export const bullMqQueueConnection = createRedisInstance(true);
+bullMqQueueConnection.on("connect", () =>
+	console.log("✅ Connected to Redis BullMQ Queue."),
+);
+
+/***
+ * Dedicated connection for BullMQ Workers (Consumers)
  */
 export const bullMqWorkerConnection = createRedisInstance(true);
-
-/***
- * Connection for BullMQ Queues (producers).
- * Queues/Producers can reuse connection of the caching instance
- */
-export const bullMqQueueConnection = redis;
-
-redis.on("connect", () => console.log("✅ Connected to Redis Cache."));
-redis.on("error", (err) => console.error("❌ Redis Cache Error: ", err));
+bullMqWorkerConnection.on("connect", () =>
+	console.log("✅ Connected to Redis BullMQ Worker."),
+);
